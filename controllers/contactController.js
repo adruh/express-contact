b4120e72ad3f18b76e533ea3f7ea5104ebdf4753
adarsh <adarshmk>
2025-12-20 07:49:50 +0530

    register,login and token handler middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,15 +2,15 @@ const asyncHandler = require('express-async-handler');
 const Contact = require('../models/contactModel');
 // @desc    Get all contacts
 // @route   GET /api/contacts
-// @access  Public
+// @access  private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+const contacts = await Contact.find({user_id:req.user.id});
     res.status(200).json(contacts);
 });
 
 // @desc    Create a new contact
 // @route   POST /api/contacts
-// @access  Public
+// @access  private
 const createContact = asyncHandler(async (req, res) => {
     console.log(req.body);
     const { name, email, phone } = req.body;
@@ -22,7 +22,8 @@ const createContact = asyncHandler(async (req, res) => {
     const contact = await Contact.create({
         name,
         email,
-        phone
+        phone,
+        user_id:req.user.id
     });
 
     res.status(201).json(contact);
@@ -30,7 +31,7 @@ const createContact = asyncHandler(async (req, res) => {
 
 // @desc    Get a contact by ID
 // @route   GET /api/contacts/:id
-// @access  Public
+// @access  private
 const getContactById = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -42,7 +43,7 @@ const getContactById = asyncHandler(async (req, res) => {
 
 // @desc    Update a contact by ID
 // @route   PUT /api/contacts/:id
-// @access  Public
+// @access  private
 const updateContactById = asyncHandler(async (req, res) => {
      const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -56,7 +57,7 @@ const updateContactById = asyncHandler(async (req, res) => {
 
 // @desc    Delete a contact by ID
 // @route   DELETE /api/contacts/:id
-// @access  Public
+// @access  private
 
 const deleteContactById = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { registerUser, loginUser, currentuser } = require("../controllers/userController");
+const validationToken = require("../middleware/validateTokenhandler");
 const router = express.Router();
 
 router.post("/register",registerUser);
 router.post("/login", loginUser);
-router.post("/current", currentuser); 
+router.post("/current",validationToken, currentuser); 
 
 module.exports = router;
